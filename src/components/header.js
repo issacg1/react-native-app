08abfd7db3265aa35e989009efd224857f986658
issacg1 <yitzygrunwald@gmail.com>
2017-11-14 13:39:58 -0500

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const Header = (props) => {
+    return (
+			<View style={styles.view}>
+				 <Text style={styles.blue}>{props.header}</Text>
+			</View>
+		);
+};
+
+const styles = StyleSheet.create({
+    blue: {
+        color: 'blue',
+				textAlign: 'center',
+				fontSize: 40,
+		},
+		view: {
+			marginTop: 20,
+			height: 20,
+			backgroundColor: '#f8f8f8',
+			zIndex: 10,
+			shadowColor: '#000',
+			shadowOffset: { width: 0, height: 2 },
+			shadowOpacity: 0.2,
+			elevation: 2,
+			position: 'relative',
+		}
+});
+
+
+export default Header;
